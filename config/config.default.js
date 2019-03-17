f85a23e83a65f5b4ccccaa15d2795d7ff0ae8389
Liuzln <943051592@qq.com>
2019-03-18 00:42:40 +0800

    init project

--- config/config.default.js
+++ config/config.default.js
@@ -6,25 +6,33 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = {};
+    /**
+     * built-in config
+     * @type {Egg.EggAppConfig}
+     **/
+    const config = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1552749927657_1634';
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1552749927657_1634';
 
-  // add your middleware config here
-  config.middleware = [];
+    // add your middleware config here
+    config.middleware = [];
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
+    // 数据库配置
+    config.sequelize = {
+        dialect: 'mysql',
+        host: '127.0.0.1',
+        port: 3306,
+        database: 'egg-sequelize-doc-default',
+    };
 
-  return {
-    ...config,
-    ...userConfig,
-  };
+    // add your user config here
+    const userConfig = {
+        // myAppName: 'egg',
+    };
+
+    return {
+        ...config,
+        ...userConfig,
+    };
 };
